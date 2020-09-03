067ef60516a64a7ecc0a505bf20e35115fae12e1
Jagdeep <jskarwal@gmail.com>
2020-09-03 22:16:11 +1000

    Project Structure Update

--- DataProducer/src/main/java/com/data/producer/camel/KafkaRouteBuilder.java
+++ DataProducer/src/main/java/com/data/producer/camel/KafkaRouteBuilder.java
@@ -3,12 +3,20 @@ package com.data.producer.camel;
 import org.apache.camel.Exchange;
 import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class KafkaRouteBuilder extends RouteBuilder {
 
     public static int messagesProcessed;
+
+    @Value("${kafkaInputTopic}")
+    private String kafkaInputTopic;
+    @Value("${kafkaBrokerHost}")
+    private String kafkaBrokerHost;
+    @Value("${kafkaBrokerPort}")
+    private String kafkaBrokerPort;
 
     Processor countProcessor = new Processor() {
         @Override
@@ -21,14 +29,55 @@ public class KafkaRouteBuilder extends RouteBuilder {
     };
     @Override
     public void configure() throws Exception {
-        System.out.println("configure Called .........");
+        System.out.println("configure Called ......... =");
         //from("stream:file?fileName=./SampleInput.txt&scanStream=true&scanStreamDelay=1000")
         from("file:.?fileName=SampleInput.txt")
                 .split(body().tokenize("\n"))
                 .streaming()
                 .setHeader("Key", constant("Camel"))
                 .process(countProcessor)
-                .to("kafka:input_topic?brokers=localhost:9092");
+                //.to("kafka:input_topic?brokers=localhost:9092");
+                .to(String.format("kafka:%s?brokers=%s:%s", kafkaInputTopic, kafkaBrokerHost, kafkaBrokerPort));
+    }
+
+    public static int getMessagesProcessed() {
+        return messagesProcessed;
+    }
+
+    public static void setMessagesProcessed(int messagesProcessed) {
+        KafkaRouteBuilder.messagesProcessed = messagesProcessed;
+    }
+
+    public String getKafkaInputTopic() {
+        return kafkaInputTopic;
+    }
+
+    public void setKafkaInputTopic(String kafkaInputTopic) {
+        this.kafkaInputTopic = kafkaInputTopic;
+    }
+
+    public String getKafkaBrokerHost() {
+        return kafkaBrokerHost;
+    }
+
+    public void setKafkaBrokerHost(String kafkaBrokerHost) {
+        this.kafkaBrokerHost = kafkaBrokerHost;
+    }
+
+    public String getKafkaBrokerPort() {
+        return kafkaBrokerPort;
+    }
+
+    public void setKafkaBrokerPort(String kafkaBrokerPort) {
+        this.kafkaBrokerPort = kafkaBrokerPort;
+    }
+
+    public Processor getCountProcessor() {
+        return countProcessor;
+    }
+
+    public void setCountProcessor(Processor countProcessor) {
+        this.countProcessor = countProcessor;
     }
 }
 
